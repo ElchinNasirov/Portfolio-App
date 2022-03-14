1fe8b6a3a732df0d2905aac600b61e5652edf721
ElchinNasirov <nasirov.elchin@gmail.com>
2022-03-13 20:24:03 -0700

    Working on Skills page, added some styling.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./Themes";
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height: 100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
 
 const Skills = () => {
     return (
-        <div>
-            Skill Component
-        </div>
+        <ThemeProvider theme={lightTheme}>
+            <Box>
+                Skill Component
+            </Box>
+        </ThemeProvider>
     )
 }
 
