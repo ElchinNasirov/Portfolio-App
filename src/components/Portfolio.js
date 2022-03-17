24256b7ee1697e05edfad433f026f8fbd696fe49
ElchinNasirov <nasirov.elchin@gmail.com>
2022-03-17 09:07:23 -0700

    Working on portfolio page, added a project card to the page.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,40 @@
 import React from "react";
 import PortfolioCard from "./PortfolioCard";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Themes";
+import { motion } from "framer-motion";
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+
+height:400vh;
+position: relative;
+display: flex;
+align-items: center;
+`
+
+const MainContainer = styled(motion.ul)`
+position: fixed;
+top: 12rem;
+left:calc(10rem + 15vw);
+height: 40vh;
+display: flex;
+color: white;
+`
+
 
 const Portfolio = () => {
     return (
-        <PortfolioCard>
-            Work Component
-        </PortfolioCard>
+        <ThemeProvider theme={darkTheme}>
+            <Box>
+                <MainContainer>
+                    <PortfolioCard>
+                        Work Component
+                    </PortfolioCard>
+                </MainContainer>
+            </Box>
+        </ThemeProvider>
     )
 }
 
--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -7,7 +7,23 @@ width: 16rem;
 height: 40vh;
 background-color: ${props => props.theme.text};
 color:${props => props.theme.body};
+
+padding: 1.5rem 2rem;
+margin-right: 8rem;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+border: 1px solid ${props => props.theme.body};
+transition: all 0.2s ease;
+
+&:hover{
+background-color: ${props => props.theme.body};
+color:${props => props.theme.text};
+border: 2px solid ${props => props.theme.text};
+}
 `
+
+
 
 const PortfolioCard = () => {
     return (
